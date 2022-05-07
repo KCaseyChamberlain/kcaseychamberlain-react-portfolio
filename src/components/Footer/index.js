fbab5effafaa9670e4a66728b6524f9a2aa38688
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-05-07 16:51:58 -0600

    footer component added to app

---
+++ src/components/Footer/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Footer() {
+
+    return (
+        <footer className="flex-row px-1">
+            <nav>
+
+                <ul className="flex-row">
+
+                    <li className={"mx-2"}>
+                        <a data-testid="github" href="https://github.com/KCaseyChamberlain" target="blank">
+                            GitHub
+                        </a>
+                    </li>
+                    <li className={"mx-2"}>
+                        <a data-testid="linkedin" href="https://www.linkedin.com/in/kcaseychamberlain" target="blank">
+                            LinkedIn
+                        </a>
+                    </li>
+                    <li className={"mx-2"}>
+                        <a data-testid="insta" href="https://www.instagram.com/khamberlain/?hl=en" target="blank">
+                            Instagram
+                        </a>
+                    </li>
+
+                </ul>
+
+            </nav>
+        </footer>
+    );
+}
+
+export default Footer;
