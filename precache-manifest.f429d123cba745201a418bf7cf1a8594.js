55ee5b0b115308c0adff3bf5190b03c3ebb653cc
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-05-08 20:49:27 -0600

    Updates

---
+++ precache-manifest.f429d123cba745201a418bf7cf1a8594.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e3ce2bc76b565976dd0",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/css/main.a38fce61.chunk.css"
+  },
+  {
+    "revision": "4e3ce2bc76b565976dd0",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/js/main.4e3ce2bc.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "05325ab30a6b8ffee9a5",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/js/2.05325ab3.chunk.js"
+  },
+  {
+    "revision": "b0636066f94373c9bdb1",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/js/runtime~main.b0636066.js"
+  },
+  {
+    "revision": "d18630c7424b220bbdee9b5534ca99ef",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/media/cover-image.d18630c7.jpg"
+  },
+  {
+    "revision": "16e4ffc09b4ef576246d305f6ed1669c",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/media/resume-image1.16e4ffc0.jpg"
+  },
+  {
+    "revision": "d3d0d01d4a68d497ed1180067e7cdb6d",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/media/resume-image2.d3d0d01d.jpg"
+  },
+  {
+    "revision": "2ec3eda2d6773d5fd4e8280a582cea61",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/media/deep-thoughts.2ec3eda2.jpg"
+  },
+  {
+    "revision": "7539bfcee06b23289f72e371eb6dc175",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/media/copy-cat-games.7539bfce.jpg"
+  },
+  {
+    "revision": "a4fcf86fd2c65f55c0bcebdf15820230",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/media/6day-six-weather.a4fcf86f.jpg"
+  },
+  {
+    "revision": "b555abdead4ab0f67349e6f85e7267ac",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/media/uhoo.b555abde.jpg"
+  },
+  {
+    "revision": "d153a1b3d99b26a7e03ef92c7decbaeb",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/media/food-festival.d153a1b3.jpg"
+  },
+  {
+    "revision": "b9faad06b8ac54500fdeaf9291a7a8d0",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/static/media/techite.b9faad06.jpg"
+  },
+  {
+    "revision": "e472f9f3b803646d08a512a02eafdb6c",
+    "url": "/KCaseyChamberlain/kcaseychamberlain-react-portfolio/index.html"
+  }
+];
