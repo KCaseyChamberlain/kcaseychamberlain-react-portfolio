b5a32287b99c77bee4bed7232432b935e92ddb4b
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-06-12 21:39:32 -0600

    download resume btn update

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,11 +6,14 @@ function Resume() {
 
     return (
         <section className="my-5" >
-            <img src={resumeImage1} className="my-2" style={{ width: "50%" }} alt="resume page 1" />
-            <img src={resumeImage2} className="my-2" style={{ width: "50%" }} alt="resume page 2" />
+            <div id='resume'>
+                <img src={resumeImage1} className="my-2 resume-images" style={{ width: "47%" }} alt="resume page 1" />
+                <img src={resumeImage2} className="my-2 resume-images" style={{ width: "47%" }} alt="resume page 2" />
+            </div>
+            
             <div className="my-2">
 
-                <div className='flex-row'>
+                <div id="download-resume" className='flex-row'>
                     <a id='download' href="https://docs.google.com/document/d/1JZmTZZerYMMK7dJCxq2Iew3pQl12C7OrLxBNVcnie2M/edit?usp=sharing" target="blank">
                         <h4>Download & View Resume Here!</h4>
                     </a>
