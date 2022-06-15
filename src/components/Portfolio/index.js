52dda81cd517da9af147d313758fed6ba9b5af08
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-06-15 17:49:46 -0600

    portfoio container given bottom padding

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Portfolio = () => {
 
     return (
-        <div className='container'>
+        <div id='container'>
             <div className="flex-row ">
 
                 {/* my budget */}
