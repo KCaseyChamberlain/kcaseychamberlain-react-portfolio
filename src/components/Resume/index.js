97773facebbb127d4fa09b913e91f24c071674b6
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-06-15 17:46:26 -0600

    resume files updates

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import resumeImage1 from "../../assets/resume/resume-image1.jpg";
 import resumeImage2 from "../../assets/resume/resume-image2.jpg";
+import resumeImage3 from "../../assets/resume/resume-image3.jpg";
 
 function Resume() {
 
     return (
         <section className="my-5" >
-            <div id='resume'>
-                <img src={resumeImage1} className="my-2 resume-images" style={{ width: "47%" }} alt="resume page 1" />
-                <img src={resumeImage2} className="my-2 resume-images" style={{ width: "47%" }} alt="resume page 2" />
+
+            <div id="dev-pro">
+                <h3 >Developer Proficiencies:</h3>
+                <p>
+                    HTML, CSS, JavaScript, MySQL, MongoDB, Express.js, React.js, Node.js, Bootstrap, Heroku, jQuery, Handlebars.js, Git.
+                </p>
             </div>
-            
+
+            <div id='resume' className='flex-row'>
+                <img src={resumeImage1} className="mx-2 resume-images" style={{ width: "31.5%" }} alt="resume page 1" />
+                <img src={resumeImage2} className="mx-2 resume-images" style={{ width: "31.5%" }} alt="resume page 2" />
+                <img src={resumeImage3} className="mx-2 resume-images" style={{ width: "31.5%" }} alt="resume page 3" />
+            </div>
+
             <div className="my-2">
                 <div id="download-resume" className='flex-row'>
                     <a id='download' href={require("../../assets/resume/KCaseyChamberlain_Resume.pdf")} download="Casey_Chamberlain_Resume.pdf">
-                        <h4>Download Resume Here!</h4>
+                        <h4>Download Resume Here</h4>
                     </a>
-                </div>
-
-                <div id="dev-pro">
-                    <h3 >Developer Proficiencies:</h3>
-                    <p>
-                        HTML, CSS, JavaScript, MySQL, MongoDB, Express.js, React.js, Node.js, Bootstrap, Heroku, jQuery, Handlebars.js, Git.
-                    </p>
                 </div>
             </div>
         </section>
