b71de9f963afca7f99c7e55a96c543cdc1edfd85
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-12-26 14:28:50 -0700

    resume updated

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import resumeImage1 from "../../assets/resume/resume-image1.jpg";
 import resumeImage2 from "../../assets/resume/resume-image2.jpg";
-import resumeImage3 from "../../assets/resume/resume-image3.jpg";
 
 function Resume() {
 
@@ -16,14 +15,13 @@ function Resume() {
             </div>
 
             <div id='resume' className='flex-row'>
-                <img src={resumeImage1} className="mx-2 resume-images" style={{ width: "31.5%" }} alt="resume page 1" />
-                <img src={resumeImage2} className="mx-2 resume-images" style={{ width: "31.5%" }} alt="resume page 2" />
-                <img src={resumeImage3} className="mx-2 resume-images" style={{ width: "31.5%" }} alt="resume page 3" />
+                <img src={resumeImage1} className="mx-2 resume-images" style={{ width: "47%" }} alt="resume page 1" />
+                <img src={resumeImage2} className="mx-2 resume-images" style={{ width: "47%" }} alt="resume page 2" />
             </div>
 
             <div className="my-2">
                 <div id="download-resume" className='flex-row'>
-                    <a id='download' href={require("../../assets/resume/KCaseyChamberlain_Resume.pdf")} download="Casey_Chamberlain_Resume.pdf">
+                    <a id='download' href={require("../../assets/resume/KCaseyChamberlain_Resume.pdf")} download="CaseyChamberlain_Resume.pdf">
                         <h4>Download Resume Here</h4>
                     </a>
                 </div>
