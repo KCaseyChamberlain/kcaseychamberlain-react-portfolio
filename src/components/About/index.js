c0c83f0694221e366c5a36ec9c579cede899aeb8
kcaseychamberlain <caseygchamberlain@gmail.com>
2023-01-01 16:02:47 -0500

    brand statement update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,14 +16,11 @@ function About() {
 
             <div id='about-block'>
                 <p className='about-text'>
-                    Recent full stack web developer graduate with previous analyst experience. Professionally educated at the University of Utah. Graduated from the University's Coding Bootcamp in the spring of 2022. Skills and experiences in HTML, CSS, JavaScript, Git, MySQL, NoSQL, Handlebars.js, and a focus on M.E.R.N. full stack web applications. Strengths in teamwork, meeting deadlines, creativity, and self reliance. Known to be a hyperactive and determined individual passionate about developing full stack web applications.
-                </p>
+                Recent full stack web developer graduate with previous analyst experience. Professionally educated at the University of Utah. Graduated from the University's Coding Bootcamp in the spring of 2022. Strengths in teamwork, meeting deadlines, creativity, and self reliance. Known to be a hyper focused and determined individual passionate about developing full stack web applications.                </p>
                 <p className='about-text'>
-                    With each project, my aim is to learn new skills and to make the most UI friendly experience. I am excited to leverage my skills as part of a fast-paced quality-driven team to build better experiences for the users who will come across my work.
+                My technical experience as an international shipping analyst for one of “America’s Best Midsize Employers” (Forbes), has taught me the importance of the corporate environment and communication. My past experience makes me a strong addition as a developer with a mindset geared towards the small details and the user.
                 </p>
-                <p className='about-text'>
-                    My technical experience as an international shipping analyst for one of “America’s Best Midsize Employers” (Forbes), has taught me the importance of the corporate environment and communication. My past experience makes me a strong addition as a developer with a mindset geared towards the small details and the user.                </p>
-            </div>zz
+            </div>
         </section>
     );
 }
